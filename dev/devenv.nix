f4e3089c3fd22b71794f73292af4028dd11a2dab
Bullish Design <BullishDesignLLC@gmail.com>
2026-05-30 13:43:27 -0400

    Updates for downstream import cleanliness

--- dev/devenv.nix
+++ dev/devenv.nix
@@ -6,4 +6,18 @@
     pkgs.git
     pkgs.uv
   ];
+
+  languages.python = {
+    enable = true;
+    package = pkgs.python313;
+    venv.enable = true;
+
+    uv = {
+      enable = true;
+      sync = {
+        enable = true;
+        allExtras = true;
+      };
+    };
+  };
 }
--- devenv.nix
+++ devenv.nix
@@ -9,18 +9,4 @@
   packages = [
     pkgs.secretspec
   ];
-
-  languages.python = {
-    enable = true;
-    package = pkgs.python313;
-    venv.enable = true;
-
-    uv = {
-      enable = true;
-      sync = {
-        enable = true;
-        allExtras = true;
-      };
-    };
-  };
 }
