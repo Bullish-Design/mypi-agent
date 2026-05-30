1af2dd756033764577461f223979cef39656feb9
Bullish Design <BullishDesignLLC@gmail.com>
2026-05-30 18:01:51 -0400

    chore: bump version to 0.3.1

--- packages/mypi-agent-cli.nix
+++ packages/mypi-agent-cli.nix
@@ -2,7 +2,7 @@
 
 python313Packages.buildPythonApplication {
   pname = "mypi-agent";
-  version = "0.3.0";
+  version = "0.3.1";
   src = lib.fileset.toSource {
     root = ../.;
     fileset = lib.fileset.unions [
